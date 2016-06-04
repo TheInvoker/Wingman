94ddfa9b259538467efb87278da7d09bb5d4e381
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2016-06-04 18:59:54 -0400

    update

---
+++ server.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var app = express();
+var pg = require('pg');
+var pgp = require('pg-promise')();
+var expressSession = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var socket = require('socket.io');
+var fs = require('fs');
+var handlebars = require('handlebars');
+var dateFormat = require('dateformat');
+var validator = require('validator');
+
+    
+var RENDER_ERROR = "Error occured on server when rendering view.";
+
+
+function isMobile(req) {
+    var ua = req.headers['user-agent'];
+    return /mobile/i.test(ua);
+}
+
+// set up view rendering using handlebars
+function renderView(sourceFile, jsonObj, callback) {
+    fs.readFile(sourceFile, function(err, data){
+        if (!err) {
+            // make the buffer into a string
+            var source = data.toString();
+            // call the render function
+            callback(200, renderToString(source, jsonObj));
+        } else {
+            // handle file read error
+            callback(500, RENDER_ERROR);
+        }
+    });
+}
+function renderToString(source, data) {
+    var template = handlebars.compile(source);
+    var outputString = template(data);
+    return outputString;
+}
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+/*
+ * Visit the home page.
+ */
+app.get('/', function (req, res) {
+	res.redirect('/welcome');
+});
+
+app.get('/welcome', function (req, res) {
+	fs.readFile(__dirname + '/res/strings.json', 'utf8', function (err, res_data) {
+		if (err) throw err;
+		var res_data = JSON.parse(res_data);
+		
+		fs.readFile(__dirname + '/res/refs.html', 'utf8', function (err, ref_data) {
+			if (err) throw err;
+			res_data["meta_js_css_references"] = ref_data;
+			
+			var filename = __dirname + (isMobile(req) ? '/views/welcome_mobile.html' : '/views/welcome.html');
+			renderView(filename, res_data, function(code, str) {
+				res.writeHead(code); 
+				res.end(str);
+			});
+		});
+	});
+});
+
+
+
+var server = app.listen(process.env.PORT || 3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Wingman started at http://%s:%s', host, port);
+});
